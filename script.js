49d823629b17c61f251b5745a02019d0b3759831
JabedH <Jabedhtusar@gmail.com>
2023-01-07 03:21:19 +0600

    Jabed - Completed open and close modal using Js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
+const showModal = document.querySelectorAll('.show-modal');
+
+const openModalFun = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+for (let i = 0; i < showModal.length; i++)
+  showModal[i].addEventListener('click', openModalFun);
+
+const closeModalFun = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+closeModal.addEventListener('click', closeModalFun);
+overlay.addEventListener('click', closeModalFun);
+
+// we can do this way also
+// overlay.addEventListener('click', function () {
+//   console.log('state:', true);
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
